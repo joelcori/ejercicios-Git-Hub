4f73172beb4c4ba1118ac14975bb804bf7706058
joelcori <joelcori111@gmail.com>
2022-03-09 16:33:51 -0500

    nuevo cambios

---
+++ ejercicio8.js
@@ -0,0 +1,38 @@
+// let i = 0;
+// while (i++ < 5){
+// 	document.write(i)
+// }
+
+// Numeros pares
+// for(let i=2; i <= 10; i++){
+// 	if (i % 2 == 0){
+// 		document.write(i + "<br>");
+// 	}
+// }
+
+// Numeros primos
+// n = parseInt(prompt("Ingrese un intervalo"));
+
+// for(let i = 0; i<n; i++){
+// }
+
+//function de edad
+// function checkAge(age){
+// 	if(age >= 18){
+// 		return "Eres mayor"
+// 	}
+// 	return "Es menor de edad";
+// }
+// document.write(checkAge(20))
+
+//function pow(a,b) potencia de un numero
+// function pow(a,b){
+// 	let potencia = a;
+// 	for(let i =1; i<b; i++){
+// 		potencia *= a;
+// 	}
+// 	return potencia;
+// }
+// document.write(pow(5, 0));
+
+	
